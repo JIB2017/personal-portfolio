cdc6982787744edd4ad442507332707522659246
JIB2017 <blacuttjuan07@gmail.com>
2023-10-10 17:01:46 -0300

    Fixed svgs position in cards for all browsers & removed unused classes

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -21,10 +21,10 @@ interface Props {
 const ProjectCard = ({ title, image, description, repository, livesite }: Props) => {
   return (
         <Card className="w-full max-w-fit !bg-transparent sm:max-w-[356px] p-4 border-slate-50">
-            <CardHeader className="flex-start flex-col mx-auto !p-0 gap-2.5 ">
-                <div className="group relative image-svg h-fit w-full filter ">
+            <CardHeader className="flex-start flex-col mx-auto !p-0 gap-2.5">
+                <div className="group relative h-fit w-full">
                     <Image src={image} alt="image-card" width={320} height={320} className="relative h-full rounded-md object-cover hover:blur-sm transition-all duration-300 cursor-pointer"/>
-                    <div className="flex-center absolute space-x-4 opacity-0 group-hover:opacity-100 top-16 left-24">
+                    <div className="flex-center absolute space-x-4 opacity-0 group-hover:opacity-100 svgcenter">
                         <Link href={repository}>
                             <AiFillGithub width={50} height={50} className="w-10 h-10 bg-gray-800 p-2 rounded-full hover:bg-gray-200 hover:text-black-200 cursor-pointer"/>
                         </Link>
