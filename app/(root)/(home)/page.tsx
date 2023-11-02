a7c743147b3e5c7c8043198707908b0d0b3e2752
JIB2017 <blacuttjuan07@gmail.com>
2023-11-02 16:10:20 -0300

    Fixed project button in hero section

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,7 +9,6 @@ import MouseTrack from "@/components/MouseTrack";
 import AboutMe from "@/components/AboutMe";
 import Footer from "@/components/Footer";
 import CTA from "@/components/CTA";
-// import Scroll from "@/components/SmoothScrolling";
 
 export const revalidate = 900;
 
@@ -23,8 +22,6 @@ const Page = async ({ searchParams }: Props) => {
     page: "1",
   });
 
-  // console.log(projects);
-
   return (
     <main className="flex-center flex-col w-full mt-20 mx-auto">
       <MouseTrack />
@@ -36,7 +33,7 @@ const Page = async ({ searchParams }: Props) => {
           </h1>
           <TypingAnimation />
           <div className="flex justify-between xs:flex-col xs:justify-center md:flex-row flex-wrap gap-12 mt-12">
-            <Btn1 text="Ver proyectos!" destiny="#projects" target="blank"/>
+            <Btn1 text="Ver proyectos!" destiny="#projects"/>
             <Btn2 text="Ver curriculum" />
           </div>
         </div>
@@ -49,14 +46,12 @@ const Page = async ({ searchParams }: Props) => {
       <Filters />
       {/* Proyectos */}
       <section className="relative flex-center flex-row text-white w-full flex-wrap gap-8 paddings mt-8 mb-32 z-10">
-        {/* <Image src="/lluvia-pixeles-sin-fondo.webp" alt="background-pixels" className="absolute" fill={true} objectFit="cover" /> */}
           {projects && projects?.map((card: {[key: string]: string}, i: number) => (
             <ProjectCard key={`${card._id}-${i.toString()}`} delay={i} title={card.title} image={card.image} technologies={card.technologies} description={card.description} repository={card.repository} livesite={card.livesite}/>
             ))}
       </section>
 
       {/* Skills */}
-      {/* <h2 id="skills" className="heading2 text-gradient-grey p-4 pb mt-20 mb-12 z-10 nonselect">Skills</h2> */}
       <Skills />
       {/* Blob effect mousetrack */}
       <div id="blob" className="xs:hidden 3xl:flex">
